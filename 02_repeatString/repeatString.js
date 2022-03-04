97bccf27e147d96a13e01d5531c8bce01dbef613
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-04 18:28:54 +0800

    added solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+function repeatString(string,num){
+    if (num > 0)
+    return string.repeat(num);
+  else if(num <0)
+  return "ERROR";
+    else
+    return "";
 
-};
+}
+
+repeatString("hey",3)
+
 
 // Do not edit below this line
 module.exports = repeatString;
