db25b78e1401d76a560e48a843fd1a808604f631
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-07 12:54:38 +0800

    code passed if you manually check console, but npm says failed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+function removeFromArray(array, toRemove) {
+    return array.filter (v =>{
+        return !toRemove.includes(v) ;
+    })
+    
 };
+
+console.log(removeFromArray([1,2,3,4,"hey"],["1",2,3,4,"hey"]))
 
 // Do not edit below this line
 module.exports = removeFromArray;
