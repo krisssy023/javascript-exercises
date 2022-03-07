6cee5a36e71d743ecd4b7e5eeee5b6bb21039017
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-07 19:30:37 +0800

    added js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
 
-};
+function sumAll(a,b){
+    const sumValues = [];
+    sumValues.push(a);
+    sumValues.push(b);
+    sumValues.sort(function(a,b){return a-b}) ;
+
+
+    let max = sumValues[1];
+    let min = sumValues[0];
+    let number ;
+
+    console.log(typeof(min));
+    console.log(typeof (max));
+    ;
+
+   
+
+    if (( min < 0 || max < 0 )||((typeof(a)) != "number"||(typeof(b)) != "number")){
+         return "ERROR"
+    } else {
+        return ((max-min)+1) * (min + max) / 2;
+    }
+
+
+}   
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
