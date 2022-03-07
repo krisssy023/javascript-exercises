171d8931f7fded26010cf2acb6acf5f5771e694f
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-07 15:35:16 +0800

    edited js , all test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,26 @@
-function removeFromArray(array, toRemove) {
-    return array.filter (v =>{
-        return !toRemove.includes(v) ;
-    })
-    
-};
 
-console.log(removeFromArray([1,2,3,4,"hey"],["1",2,3,4,"hey"]))
+function removeFromArray(arr,...toRemove){
+
+   
+   let removed = arr.filter( item => toRemove.includes(item));
+
+   for (let i = 0 ; i < arr.length ; i++){
+       for (let j=0 ; j <removed.length ; j++){
+
+        if ( arr [i]== removed[j]){
+            arr.splice (i,1);
+            
+        }
+       }
+      
+   }
+
+ return arr ;
+}
+
+
+removeFromArray([1,2,2,3,"hey"],"1",2,3,"hey")
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
