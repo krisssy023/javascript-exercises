ec25d9b63402d5461dbbf3893c412b78dc568ce7
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-08 16:14:15 +0800

    added js code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,31 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  num = (num-32) * (5/9);
+  
 
+    if (num != 0){
+      num = num.toFixed(1) ;
+      return (Number(num))
+    }
+    else {
+      num = num.toFixed() ;
+      return (Number(num))
+    }
+      
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  num = num *(9/5) + 32 ;
 
+  if (num != 0){
+    num = num.toFixed(1) ;
+    return (Number(num))
+  }
+  else {
+    num = num.toFixed() ;
+    return (Number(num))
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
