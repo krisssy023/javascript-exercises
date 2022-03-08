2179bd38f002109786d027132df6955ab0914f14
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-08 18:50:07 +0800

    added js code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
+const add = function(a,b) {
+  let sum = a + b ;
+  return sum ;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  let difference = a - b ;
+  return difference ;
 	
 };
 
-const sum = function() {
-	
+const sum = function(valuesToSum) {
+  let sumOfValues ;
+  sumOfValues = valuesToSum.reduce((partialSum, a) => partialSum + a, 0);
+   return sumOfValues;
+}
+
+
+
+const multiply = function(valuesToMultiply) {
+
+  let values = valuesToMultiply;
+  let product= 1;
+
+  for (let i = 0 ; i < values.length ;i++){
+    product *= values[i] ;
+  }
+  return product ;
+}
+
+
+const power = function(a,b) {
+	let result = Math.pow (a,b) ;
+  return result ;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let answer = 1;
+    if (n == 0 || n == 1){
+     return answer;
+    }else{
+      for(var i = n; i >= 1; i--){
+        answer = answer * i;
+    }
+    return answer;
+  }  
 };
 
 // Do not edit below this line
