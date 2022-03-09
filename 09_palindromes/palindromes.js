bc8760e4dff1c0bb8c806fd12a354a4f88128342
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-09 11:56:58 +0800

    added js code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str = str.replace(/\s+/g, ''); // removes whitespace
+    let stringNoPunctuation = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
-};
+    let stringToLowerCase = stringNoPunctuation.toLowerCase() ;
 
+    let reverseString = stringToLowerCase.split("").reverse().join("");
+    
+        if (stringToLowerCase === reverseString){
+            return true ;
+        } else {
+            return false ;
+        }
+}
+
+console.log(palindromes("racecar!"))
 // Do not edit below this line
 module.exports = palindromes;
