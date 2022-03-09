58373b9354b2a4db8954043b34f4d210b3ba4460
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-09 13:21:25 +0800

    added js code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibSequence = [ 0, 1 ];
 
+        if (num < 0){
+            return "OOPS"
+        } else {
+            for (let i = 2 ; i < num + 1 ; i++){
+
+                 fibSequence.push(fibSequence[i-2]+ fibSequence [i-1]);
+            }
+            return (fibSequence[num]);
+        }
+      
 };
 
 // Do not edit below this line
