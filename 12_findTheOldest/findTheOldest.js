463666816ea9d978fbd7c9846dc734470b9ec8a5
krisssy023 <95204270+krisssy023@users.noreply.github.com>
2022-03-14 14:26:15 +0800

    added code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+    let peopleWithAge = people ;
+    let peopleAgeList = [];
+
+   
+
+ peopleWithAge.forEach(object => { //inserts age key
+        object.age = '';
+        });
+
+     for (let i = 0; i <peopleWithAge.length; i++){ 
+
+        function doesKeyExists(obj,key){
+            return key in obj;
+        }
+         if ((doesKeyExists(peopleWithAge[i],"yearOfDeath")) === false){
+             peopleWithAge[i].yearOfDeath = new Date().getFullYear()
+
+        }
+     }
+
+
+   for (let i = 0; i <peopleWithAge.length; i++){  //inserts age value
+      age = ((people[i].yearOfDeath) -(people[i].yearOfBirth)) ;
+      peopleAgeList.push(age) ;
+      peopleWithAge[i].age = age ;
+      
+     }
+
+    peopleWithAge.sort((a, b) => {
+        return b.age - a.age;
+    });
+
+    let oldest = peopleWithAge[0] ;
+    return oldest ;
 };
-
+ 
 // Do not edit below this line
 module.exports = findTheOldest;
